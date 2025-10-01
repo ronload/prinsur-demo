7eac8480a4b2ab0688fef4159937d1214fd3c4db
ronload <91997734+ronload@users.noreply.github.com>
2025-10-02 00:11:23 +0800

    feat: Add open graph

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -42,6 +42,26 @@ export const metadata: Metadata = {
       },
     ],
   },
+  openGraph: {
+    title: "Prinsur.com",
+    description: "Your insurance, your choice.",
+    url: "https://prinsur.com",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Prinsur - Smart Insurance Matching Preview"
+      },
+    ],
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Prinsur.com",
+    description: "Your insurance, your choice.",
+    images: ["/opengraph-image.png"],
+  },
 };
 
 export const viewport: Viewport = {

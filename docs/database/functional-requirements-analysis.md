a627f60bafea097e9d779e16cdb893632189a9cd
ronload <91997734+ronload@users.noreply.github.com>
2025-11-01 20:59:38 +0800

    feat: enable google analyse

--- docs/database/functional-requirements-analysis.md
+++
@@ -1,6 +0,0 @@
-| usage          | encrypt-algo |
-| -------------- | ------------ |
-| password       | Argon2id     |
-| search_hash    | HMAC-SHA256  |
-| session_token  | SHA-256      |
-| sensitive_info | AES-256-GCM  |
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
@@ -103,6 +104,33 @@ export default async function LocaleLayout({
         <meta name="apple-mobile-web-app-title" content="Prinsur" />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKTR23VN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TKTR23VN');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <ErrorBoundary>
           <ThemeProvider
             attribute="class"
